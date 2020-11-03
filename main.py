3c4059f34fe04297426acab7aa5577ab199120f0
Dennis Idowu <dennis_idowu@live.com>
2020-11-02 19:06:15 -0500

    added comment regarding use of requests for future reference

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from urllib import request
 
-"""Making API call and working with json objects for random jokes"""
+"""Making API call and working with json objects for random jokes. 
+Removal of s in http deters use of SSL certification"""
 url = "http://official-joke-api.appspot.com/random_ten"
 r = request.urlopen(url)
 """Printing the http request, getcode to receive 200 status and read to read content:"""
