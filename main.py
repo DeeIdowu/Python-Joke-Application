c44f73889c97d32e485a06553b42ea7153b8970b
Dennis Idowu <dennis_idowu@live.com>
2020-11-02 19:05:20 -0500

    configured file to run 10 random jokes and display json objects

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from urllib import request
 
-r = request.urlopen("http://www.google.com")
+"""Making API call and working with json objects for random jokes"""
+url = "http://official-joke-api.appspot.com/random_ten"
+r = request.urlopen(url)
 """Printing the http request, getcode to receive 200 status and read to read content:"""
 print(r.getcode())
 print(r.read())
