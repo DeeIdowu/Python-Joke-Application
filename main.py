3b9af4d09f8f3a9220d53979f173bae95bf0d8fe
Dennis Idowu <dennis_idowu@live.com>
2020-11-02 19:00:28 -0500

    Added getcode to recieve 200 status upon get request and .read method to read content of get request

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from urllib import request
 
 r = request.urlopen("http://www.google.com")
-"""Printing the  http request:"""
-print(r)
+"""Printing the http request, getcode to receive 200 status and read to read content:"""
+print(r.getcode())
+print(r.read())
