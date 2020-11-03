a42b3946ca992870ddf1fa0b3a8d137ec82d1dc6
Dennis Idowu <dennis_idowu@live.com>
2020-11-02 19:09:22 -0500

    imported json andcreated logic to display json data in terminal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from urllib import request
+import json
 
 """Making API call and working with json objects for random jokes. 
 Removal of s in http deters use of SSL certification"""
@@ -6,4 +7,6 @@ url = "http://official-joke-api.appspot.com/random_ten"
 r = request.urlopen(url)
 """Printing the http request, getcode to receive 200 status and read to read content:"""
 print(r.getcode())
-print(r.read())
+data = r.read()
+jsonData = json.loads(data)
+print(jsonData)
